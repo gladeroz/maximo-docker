1146c2842c412569efc4f93bfb074c2ddd3d8a7e
gladeroz <julien.barreault@atos.net>
2018-08-01 20:49:08 +0200

    JBR : commit 7.6.1 avec iot18 skin

--- maximo/startinstall.sh
+++ maximo/startinstall.sh
@@ -106,7 +106,7 @@ EOF
 sleep 10
 
 /opt/IBM/SMP/ConfigTool/scripts/reconfigurePae.sh -action updateApplication \
-    -updatedb -deploymaximoear -enableSkin tivoli13 -enableEnhancedNavigation
+    -updatedb -deploymaximoear -enableSkin iot18 -enableEnhancedNavigation
     
 # Stop all application servers
 /opt/IBM/SMP/ConfigTool/wasclient/ThinWsadmin.sh -lang jython \
