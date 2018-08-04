0bcc1de3f89cb1f49f731befca0b4c3ff91fc930
gladeroz <julien.barreault@atos.net>
2018-08-05 01:02:02 +0200

    JBR : demo sample

--- maximo/startinstall.sh
+++ maximo/startinstall.sh
@@ -105,16 +105,14 @@ EOF
 
 sleep 10
 
-/opt/IBM/SMP/ConfigTool/scripts/reconfigurePae.sh -action updateApplication \
+/opt/IBM/SMP/ConfigTool/scripts/reconfigurePae.sh -deployDemoData -action updateApplication \
     -updatedb -deploymaximoear -enableSkin IoT18 -enableEnhancedNavigation
     
 # Stop all application servers
-/opt/IBM/SMP/ConfigTool/wasclient/ThinWsadmin.sh -lang jython \
-    -username "$DMGR_ADMIN_USER" -password "$DMGR_ADMIN_PASSWORD" \
-    -f /opt/StopAllServers.py
-    
-/opt/IBM/SMP/ConfigTool/scripts/reconfigurePae.sh -action deployDatabaseConfiguration -deployDemoData
-    
+#/opt/IBM/SMP/ConfigTool/wasclient/ThinWsadmin.sh -lang jython \
+#    -username "$DMGR_ADMIN_USER" -password "$DMGR_ADMIN_PASSWORD" \
+#    -f /opt/StopAllServers.py
+
 # Start all application servers ... sometimes to fail to start servers during updateApplicaton task
 /opt/IBM/SMP/ConfigTool/wasclient/ThinWsadmin.sh -lang jython \
     -username "$DMGR_ADMIN_USER" -password "$DMGR_ADMIN_PASSWORD" \
