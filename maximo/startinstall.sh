392bba264d4d3201928aa285d222caa724fc7a38
gladeroz <julien.barreault@atos.net>
2018-08-05 02:07:57 +0200

    JBR : add note

--- maximo/startinstall.sh
+++ maximo/startinstall.sh
@@ -91,7 +91,9 @@ EOF
 
 # Run Configuration Tool
 /opt/IBM/SMP/ConfigTool/scripts/reconfigurePae.sh -action deployConfiguration \
-    -inputfile $CONFIG_FILE -automatej2eeconfig -deployDemoData
+    -inputfile $CONFIG_FILE -automatej2eeconfig
+    
+/opt/IBM/SMP/ConfigTool/scripts/reconfigurePae.sh -action deployDatabaseConfiguration -deployDemoData
 
 # Add 80 and 443 to maximo_host
 /opt/IBM/SMP/ConfigTool/wasclient/ThinWsadmin.sh -lang jython \
